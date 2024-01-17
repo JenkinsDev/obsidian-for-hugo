5d94be52330ffda82ddd791c42738906cdbd7007
David Jenkins <djenkinsdev@gmail.com>
2024-01-17 11:39:19 -0500

    Clean up a bit of the code, and introduce git log controlled modification timestamps

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/jenkinsdev/obsidian-for-hugo
 
 go 1.21.4
 
-require github.com/adrg/frontmatter v0.2.0
-
 require (
+	github.com/adrg/frontmatter v0.2.0
+	gopkg.in/yaml.v2 v2.4.0
 	github.com/BurntSushi/toml v0.3.1 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	github.com/creack/pty v1.1.21 // indirect
 )
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
   "fmt"
   "flag"
+  "io/ioutil"
   "os"
+  "os/exec"
   "path"
   "regexp"
   "strings"
+  "strconv"
   "sync"
   "time"
 
   "github.com/adrg/frontmatter"
+  "github.com/creack/pty"
   "gopkg.in/yaml.v2"
 )
 
@@ -31,42 +35,84 @@ type FrontMatter struct {
   Slug string `yaml:"slug"`
 }
 
-type ContentProcessor = func(string, []byte) []byte
+type File struct {
+  Path string
+  Name string
+  Title string
+  Contents []byte
+}
+
+type FrontMatterProcessor = func(Config, File, *FrontMatter)
+type ContentProcessor = func(Config, File, []byte) []byte
+
 type Config struct {
   VaultDir string
   OutputDir string
+  FrontMatterProcessors []FrontMatterProcessor
   ContentProcessors []ContentProcessor
 }
 
-func convertFrontMatter(fileName string, contents []byte) []byte {
-  var frontMatter FrontMatter
-
-  stringReader := strings.NewReader(string(contents))
-  rest, _ := frontmatter.Parse(stringReader, &frontMatter)
-
+func addFallbackFrontMatterTitle(config Config, file File, frontMatter *FrontMatter) {
   if frontMatter.Title == "" {
-    frontMatter.Title = strings.ReplaceAll(fileName, "#", "")
+    frontMatter.Title = file.Title
   }
+}
 
+func addFallbackFrontMatterSlug(config Config, file File, frontMatter *FrontMatter) {
   if frontMatter.Slug == "" {
     slugifiedTitle := slugifyRegex.ReplaceAllString(frontMatter.Title, "-")
     frontMatter.Slug = strings.ToLower(slugifiedTitle)
   }
-
-  if frontMatter.Date == "" {
-    fileInfo, err := os.Stat(fileName)
-    if err != nil {
-      frontMatter.Date = time.Now().Format(time.RFC3339)
-    } else {
-      frontMatter.Date = fileInfo.ModTime().Format(time.RFC3339)
-    }
-  }
-
-  marshalled, _ := yaml.Marshal(frontMatter)
-  return []byte(fmt.Sprintf("---\n%s---\n%s", marshalled, string(rest)))
 }
 
-func convertContent(fileName string, contents []byte) []byte {
+func attemptGitDate(config Config, file File, frontMatter *FrontMatter) {
+  gitRoot, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+  if err != nil {
+    return
+  }
+
+  command := exec.Command("git", "--no-pager", "log", "-1", "--format=%ad", "--date=unix",  "--", file.Path)
+  command.Dir = string(gitRoot)
+
+  ptmx, err := pty.Start(command)
+  if err != nil {
+    return
+  }
+
+  defer ptmx.Close()
+  output, err := ioutil.ReadAll(ptmx)
+
+  if err != nil {
+    timestamp := strings.TrimSpace(string(output))
+    i, _ := strconv.ParseInt(timestamp, 10, 64)
+    frontMatter.Date = time.Unix(i, 0).Format(time.RFC3339)
+  }
+}
+
+func attemptFileDate(config Config, file File, frontMatter *FrontMatter) {
+  fileInfo, err := os.Stat(file.Path)
+  if err != nil {
+    return
+  }
+
+  frontMatter.Date = fileInfo.ModTime().Format(time.RFC3339)
+}
+
+func addFallbackFrontMatterDate(config Config, file File, frontMatter *FrontMatter) {
+  if frontMatter.Date == "" {
+    attemptGitDate(config, file, frontMatter)
+  }
+
+  if frontMatter.Date == "" {
+    attemptFileDate(config, file, frontMatter)
+  }
+  
+  if frontMatter.Date == "" {
+    frontMatter.Date = time.Now().Format(time.RFC3339)
+  }
+}
+
+func convertWikiLinks(config Config, file File, contents []byte) []byte {
   contents = wikiLinkRegex.ReplaceAllFunc(contents, func(match []byte) []byte {
     link := string(match[2:len(match)-2])
 
@@ -85,28 +131,64 @@ func convertContent(fileName string, contents []byte) []byte {
   return contents
 }
 
-func convertFile(fromPath string, toPath string, contentProcessors []ContentProcessor) {
+/// Parses the frontmatter from the file, returns the frontmatter and the rest of the
+/// file's contents.
+func parseFrontMatter(config Config, file File) (FrontMatter, []byte, error) {
+  var frontMatter FrontMatter
+
+  stringReader := strings.NewReader(string(file.Contents))
+  rest, err := frontmatter.Parse(stringReader, &frontMatter)
+  if err != nil {
+    return frontMatter, rest, err
+  }
+
+  return frontMatter, rest, nil
+}
+
+func marshalFrontMatter(frontMatter *FrontMatter) []byte {
+  marshalled, _ := yaml.Marshal(frontMatter)
+  return []byte(fmt.Sprintf("---\n%s---", marshalled))
+}
+
+func convertFile(config Config, fromPath string, toPath string) error {
+  var file File
   defer wg.Done()
 
   contents, err := os.ReadFile(fromPath)
   if err != nil {
-    return
+    return err
   }
 
-  fileName := path.Base(fromPath)
-  fileName = strings.ReplaceAll(fileName, ".md", "")
+  file.Path = fromPath
+  file.Contents = contents
+  file.Name = path.Base(fromPath)
+  file.Title = strings.ReplaceAll(strings.ReplaceAll(file.Name, ".md", ""), "#", "")
 
-  for _, processor := range contentProcessors {
-    contents = processor(fileName, contents)
-  }
-
-  err = os.WriteFile(toPath, contents, 0644)
+  frontMatter, body, err := parseFrontMatter(config, file)
   if err != nil {
-    return
+    return err
   }
+
+  for _, processor := range config.ContentProcessors {
+    body = processor(config, file, body)
+  }
+
+  for _, processor := range config.FrontMatterProcessors {
+    processor(config, file, &frontMatter)
+  }
+
+  marshalledFrontMatter := marshalFrontMatter(&frontMatter)
+  file.Contents = []byte(fmt.Sprintf("%s\n%s", marshalledFrontMatter, string(body)))
+
+  err = os.WriteFile(toPath, file.Contents, 0644)
+  if err != nil {
+    return err
+  }
+
+  return nil
 }
 
-func convertAllRecursively(fromDirPath string, toDirPath string, contentProcessors []ContentProcessor) error {
+func convertAllRecursively(config Config, fromDirPath string, toDirPath string) error {
   var err error
 
   files, err := os.ReadDir(fromDirPath)
@@ -129,13 +211,13 @@ func convertAllRecursively(fromDirPath string, toDirPath string, contentProcesso
         return err
       }
 
-      err := convertAllRecursively(fromFullPath, toFullPath, contentProcessors)
+      err := convertAllRecursively(config, fromFullPath, toFullPath)
       if err != nil {
         return err
       }
     } else {
       wg.Add(1)
-      go convertFile(fromFullPath, toFullPath, contentProcessors)
+      go convertFile(config, fromFullPath, toFullPath)
     }
   }
 
@@ -143,12 +225,18 @@ func convertAllRecursively(fromDirPath string, toDirPath string, contentProcesso
 }
 
 func ConvertObsidianToHugo(config Config) error {
-  err := convertAllRecursively(config.VaultDir, config.OutputDir, config.ContentProcessors)
+  // clean up the output directory
+  err := os.RemoveAll(config.OutputDir)
   if err != nil {
     return err
   }
 
-  return nil
+  err = os.MkdirAll(config.OutputDir, 0755)
+  if err != nil {
+    return err
+  }
+
+  return convertAllRecursively(config, config.VaultDir, config.OutputDir)
 }
 
 func main() {
@@ -157,28 +245,26 @@ func main() {
   if *help {
     flag.PrintDefaults()
     os.Exit(0)
-  }
-
-  if *vaultDir == "" {
+  } else if *vaultDir == "" {
     fmt.Println("Obsidian Vault path is required")
     os.Exit(1)
-  }
-
-  if *outputDir == "" {
+  } else if *outputDir == "" {
     fmt.Println("Hugo Content path is required")
     os.Exit(1)
   }
 
-  config := Config{
+  err := ConvertObsidianToHugo(Config{
     VaultDir: *vaultDir,
     OutputDir: *outputDir,
-    ContentProcessors: []ContentProcessor{
-      convertFrontMatter,
-      convertContent,
+    FrontMatterProcessors: []FrontMatterProcessor{
+      addFallbackFrontMatterTitle,
+      addFallbackFrontMatterSlug,
+      addFallbackFrontMatterDate,
     },
-  }
-
-  err := ConvertObsidianToHugo(config)
+    ContentProcessors: []ContentProcessor{
+      convertWikiLinks,
+    },
+  })
 
   if err != nil {
     fmt.Println(err)
